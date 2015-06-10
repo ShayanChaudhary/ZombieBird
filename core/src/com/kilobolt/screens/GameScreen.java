c1c0a48b89889803ba4ebc9fe80487c3a9df88a0
Shayan Chaudhary <sc12811@my.bristol.ac.uk>
2015-06-10 19:23:36 +0100

    basic framework done

---
+++ core/src/com/kilobolt/screens/GameScreen.java
@@ -0,0 +1,69 @@
+package com.kilobolt.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.kilobolt.gameworld.GameRenderer;
+import com.kilobolt.gameworld.GameWorld;
+
+
+/**
+ * Created by Shayan on 10/06/2015.
+ */
+public class GameScreen implements Screen {
+
+    /*Game world and render objects*/
+    private GameWorld world;
+    private GameRenderer renderer;
+
+    public GameScreen(){
+        Gdx.app.log("GameScreen", "Attached");
+        world = new GameWorld(); // initialize world
+        renderer = new GameRenderer(world); // initialize renderer
+    }
+
+    @Override
+    public void show() {
+        Gdx.app.log("GameScreen", "show called");
+    }
+
+    @Override
+    public void render(float delta) {
+
+        // Sets a Color to Fill the Screen with (RGB = 10, 15, 230), Opacity of 1 (100%)
+        Gdx.gl.glClearColor(10 / 255.0f, 15 / 255.0f, 230 / 255.0f, 1f);
+        // Fills the screen with the selected color
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        // Covert Frame rate to String, print it
+        //Gdx.app.log("GameScreen FPS", (1 / delta) + "");
+
+        // We are passing in delta to our update method to perform frame-rate independent movement.
+        world.update(delta);
+        renderer.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        Gdx.app.log("GameScreen", "resizing");
+    }
+
+    @Override
+    public void pause() {
+        Gdx.app.log("GameScreen", "pause called");
+    }
+
+    @Override
+    public void resume() {
+        Gdx.app.log("GameScreen", "resume called");
+    }
+
+    @Override
+    public void hide() {
+        Gdx.app.log("GameScreen", "hide called");
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
